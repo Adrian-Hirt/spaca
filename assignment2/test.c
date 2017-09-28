c39d422f142d1c77018f23a2bfc4e8de41cd71bb
Adrian Hirt <aedu.hirt@gmail.com>
2017-09-28 17:07:24 +0200

    Almost working, i guess

--- assignment2/test.c
+++ assignment2/test.c
@@ -1,6 +1,16 @@
 #include "assignment2.h"
 
+void test_reverse();
+void test_whitespace();
+
+int main() {
+	test_reverse();
+	test_whitespace();
+	return 0;
+}
+
 void test_reverse() {
+	printf("---------- Test Reverse ----------\n");
 	uint32_t array1[] = {1, 2, 3, 4, 5, 6, 7, 8};
 	unsigned int len1 = 8;
 	reverse_array(array1, len1);
@@ -8,18 +18,19 @@ void test_reverse() {
 
 	for(int i = 0; i < len1; i++) {
 		if(array1[i] != array1_swp[i]) {
-			printf("reverse_array: Error! Value is %i, but should be %i \n", array1[i], array1_swp[i]);
+			printf("Error! Value is %i, but should be %i \n", array1[i], array1_swp[i]);
 			return;
 		}
 	}
-	printf("reverse_array: Test done, all correct :) \n");
+	printf("Test done, all correct :) \n");
 }
 
 void test_whitespace() {
-
-}
-
-int main() {
-	test_reverse();
-	return 0;
+	printf("---------- Test Whitespace ----------\n");
+	char *str = "Lorem ipsum dolor sit amet";
+	char *ptr = "";
+	char **output = &ptr;
+	int res = first_word(str, output);
+	printf("Length of the word is %i \n", res);
+	printf("Word is %p \n", &output); 
 }
--- assignment2/whitespace_separated_word.c
+++ assignment2/whitespace_separated_word.c
@@ -9,12 +9,15 @@ int first_word(char *input_string, char **output_word) {
 		current = *(input_string + length);
 	}
 
-	char *result = malloc(length + 1);
+	char *result = malloc(5000);
 
 	for(int i = 0; i < length; i++) {
 		result[i] = *(input_string + i);
 	}
 
 	output_word = &result;
+
+	printf("The word is %s \n", result);
+	printf("The address is %p \n", &result);
 	return length;
 }
